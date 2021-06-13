60f74879b2bf2896938b9e30a917a82394d8c4be
rnimisha <nimisaraut.gmail.com>
2021-06-13 20:07:43 +0545

    health page modified

--- website/js/javascript.js
+++ website/js/javascript.js
@@ -1,3 +1,5 @@
+
+
 // open button click on new tab
 function NewTab() 
 {
@@ -14,8 +16,59 @@ function NewTabAdopt()
 function NewTabTrain() 
 {
   window.open("about.html#trainings", "_blank");
-}
+}a
 function NewTabHealth() 
 {
   window.open("health.html", "_blank");
+}
+
+//change image on hover
+function changeImage1(){
+    document.getElementById("changeimg1").src="images/change1.jpg";
+}
+function changeBack1(){
+    document.getElementById("changeimg1").src="images/adopt1.jpg";
+}
+function changeImage2(){
+    document.getElementById("changeimg2").src="images/change2.jpg";
+}
+function changeBack2(){
+    document.getElementById("changeimg2").src="images/adopt2.jpg";
+}
+function changeImage3(){
+    document.getElementById("changeimg3").src="images/change3.jpg";
+}
+function changeBack3(){
+    document.getElementById("changeimg3").src="images/adopt3.jpg";
+}
+function changeImage4(){
+    document.getElementById("changeimg4").src="images/change4.jpg";
+}
+function changeBack4(){
+    document.getElementById("changeimg4").src="images/adopt4.jpg";
+}
+function changeImage5(){
+    document.getElementById("changeimg5").src="images/change5.jpg";
+}
+function changeBack5(){
+    document.getElementById("changeimg5").src="images/adopt5.jpg";
+}
+function changeImage6(){
+    document.getElementById("changeimg6").src="images/change6.jpg";
+}
+function changeBack6(){
+    document.getElementById("changeimg6").src="images/adopt6.jpg";
+}
+
+//view more 
+function moreView() {
+  var shows = document.getElementById("showmore");
+  if (!shows.style.display ||shows.style.display == "none") {
+    shows.style.display = "flex";
+    document.getElementById("showbutton").innerHTML = "Show Less";
+  } else {
+    shows.style.display = "none";
+    document.getElementById("showbutton").innerHTML = "Show More";
+  }
+
 }
