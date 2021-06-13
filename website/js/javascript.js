6a94e35d0165947257f6c5e899691e1f94c2c9e0
rnimisha <nimisaraut.gmail.com>
2021-06-13 19:42:56 +0545

    bootstrap carousel

---
+++ website/js/javascript.js
@@ -0,0 +1,21 @@
+// open button click on new tab
+function NewTab() 
+{
+  window.open("contact.html", "_blank");
+}
+function NewTabProduct() 
+{
+  window.open("about.html#products", "_blank");
+}
+function NewTabAdopt() 
+{
+  window.open("health.html#adopts", "_blank");
+}
+function NewTabTrain() 
+{
+  window.open("about.html#trainings", "_blank");
+}
+function NewTabHealth() 
+{
+  window.open("health.html", "_blank");
+}
