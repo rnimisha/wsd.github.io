e65f4fa3013a6e2fbcf429f23cd919931dffc15b
rnimisha <nimisaraut.gmail.com>
2021-06-25 17:17:47 +0545

    dynamic shopping list done

--- labs/dynamic-shopping-list/main.js
+++ labs/dynamic-shopping-list/main.js
@@ -1,2 +1,29 @@
-let add=document.getElementById("#addItem");
+let add=document.getElementById("addItem");
 let item=document.getElementById("items");
+
+function addmoreitem(){
+	let text=document.getElementById("listname").value;
+	let listtext=document.createTextNode(text);
+	let list=document.createElement("li");
+
+	//deleting button
+	let delbtn=document.createElement("button");
+	delbtn.textContent="Delete";
+
+	//new item with delete button
+	item.appendChild(list);
+	list.appendChild(listtext);
+	list.appendChild(delbtn);
+
+	//delete item
+	delbtn.onclick=function()
+	{
+		item.removeChild(list);
+	}
+}
+
+//admoredata on click
+if(add){
+  add.addEventListener("click", addmoreitem, false);
+}
+
